60f3f72d47c28bb99b958d4f4b0a995edd2209c9
Doriann <d.xavier@lahorde.school>
2025-03-20 19:51:06 +0100

    removing bad script

--- Source/PortalProject/PortalSurfaces/DynamicWall.cpp
+++
@@ -1,2 +0,0 @@
-﻿#include "C:\Users\dodo\Documents\Unreal Projects\PortalProject\Intermediate\Build\Win64\x64\PortalProjectEditor\Development\UnrealEd\SharedPCH.UnrealEd.Project.ValApi.Cpp20.h"
-#include "DynamicWall.h"
